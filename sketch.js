aa44a0bfaa4eaa0970865ceed6c37a118b31b4ac
Ahsoka23 <alexandraabregolopez@gmail.com>
2021-12-16 16:18:40 -0600

    Creación de un segundo suelo

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var Ground2; 
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,6 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    Ground2= new Ground(100,100,10,10);
 
 }
 
@@ -59,4 +61,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    Ground2.display();
 }
